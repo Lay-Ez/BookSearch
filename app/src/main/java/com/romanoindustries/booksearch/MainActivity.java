c0529df17b04377f47069ef7d4cd1aa36e13cc83
Roman <deep.depp@yandex.ru>
2020-03-06 20:42:48 +0300

    add tabs for navigation

--- app/src/main/java/com/romanoindustries/booksearch/MainActivity.java
+++ app/src/main/java/com/romanoindustries/booksearch/MainActivity.java
@@ -7,11 +7,14 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private Toolbar toolbar;
     private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,8 @@ public class MainActivity extends AppCompatActivity {
         PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),
                 FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         viewPager.setAdapter(pagerAdapter);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/com/romanoindustries/booksearch/PagerAdapter.java
+++ app/src/main/java/com/romanoindustries/booksearch/PagerAdapter.java
@@ -1,10 +1,12 @@
 package com.romanoindustries.booksearch;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.romanoindustries.booksearch.fragments.SavedBooksFragment;
 import com.romanoindustries.booksearch.fragments.SearchFragment;
 
 public class PagerAdapter extends FragmentPagerAdapter {
@@ -16,11 +18,25 @@ public class PagerAdapter extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return new SearchFragment();
+        if (position == 0) {
+            return new SearchFragment();
+        } else {
+            return new SavedBooksFragment();
+        }
     }
 
     @Override
     public int getCount() {
-        return 1;
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return "Search";
+        } else {
+            return "Saved";
+        }
     }
 }
