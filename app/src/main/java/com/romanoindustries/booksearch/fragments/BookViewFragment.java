e5d2365cb742e29a8a0996edebb335c6f40f586d
Roman <deep.depp@yandex.ru>
2020-03-10 20:26:45 +0300

    add initial layout for viewing individual books (also adjust overall spacing throughout the app)

--- app/src/main/java/com/romanoindustries/booksearch/fragments/BookViewFragment.java
+++ app/src/main/java/com/romanoindustries/booksearch/fragments/BookViewFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -14,14 +15,27 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.romanoindustries.booksearch.R;
 import com.romanoindustries.booksearch.bookmodel.Book;
+import com.romanoindustries.booksearch.bookmodel.VolumeInfo;
+import com.romanoindustries.booksearch.imagetransformation.RoundedCornersTransformation;
 import com.romanoindustries.booksearch.viewmodels.BookViewActivityViewModel;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import java.util.List;
 
 
 public class BookViewFragment extends Fragment {
     private static final String TAG = "BookViewFragment";
     private BookViewActivityViewModel viewModel;
 
-    private TextView title_tv;
+    private TextView titleTV;
+    private ImageView thumbnailIM;
+    private TextView authorTV;
+    private TextView publisherTV;
+    private TextView ratingTv;
+    private TextView numReviewsTv;
+    private TextView numPagesTv;
+    private TextView reviewsLabelTv;
 
     public BookViewFragment() {
         // Required empty public constructor
@@ -32,6 +46,7 @@ public class BookViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_book_view, container, false);
+        initViews(view);
 
         viewModel = new ViewModelProvider(requireActivity()).get(BookViewActivityViewModel.class);
         viewModel.getBook().observe(getViewLifecycleOwner(), new Observer<Book>() {
@@ -41,6 +56,7 @@ public class BookViewFragment extends Fragment {
                     Log.d(TAG, "onChanged: book is now null");
                 } else {
                     Log.d(TAG, "onChanged: book is now " + book.getVolumeInfo().getTitle());
+                    displayBook(book);
                 }
             }
         });
@@ -48,4 +64,49 @@ public class BookViewFragment extends Fragment {
         return view;
     }
 
+    private void displayBook(Book book) {
+        VolumeInfo volumeInfo = book.getVolumeInfo();
+
+        loadThumbnail(volumeInfo.getThumbnailURL());
+        titleTV.setText(volumeInfo.getTitle());
+
+        List<String> authors = volumeInfo.getAuthors();
+        for (int i = 0; i < authors.size(); i++) {
+            if (i == authors.size() - 1) {
+                authorTV.append(authors.get(i));
+            } else {
+                authorTV.append(authors.get(i) + ", ");
+            }
+        }
+
+        publisherTV.setText(volumeInfo.getPublisher());
+        ratingTv.setText(String.valueOf(volumeInfo.getAvgRating()));
+        numPagesTv.setText(String.valueOf(volumeInfo.getPageCount()));
+
+        int countReviews = volumeInfo.getRatingsCount();
+        numReviewsTv.setText(String.valueOf(countReviews));
+        if (countReviews == 1) {
+            reviewsLabelTv.setText(getString(R.string.review));
+        } else {
+            reviewsLabelTv.setText(getString(R.string.reviews));
+        }
+    }
+
+    private void loadThumbnail(String url) {
+        int radius = 5;
+        int margin = 5;
+        Transformation transformation = new RoundedCornersTransformation(radius, margin);
+        Picasso.get().load(url).transform(transformation).into(thumbnailIM);
+    }
+
+    private void initViews(View view) {
+        titleTV = view.findViewById(R.id.title_tv);
+        thumbnailIM = view.findViewById(R.id.thumbnail_iv);
+        authorTV = view.findViewById(R.id.author_tv);
+        publisherTV = view.findViewById(R.id.publisher_tv);
+        ratingTv = view.findViewById(R.id.rating_tv);
+        numReviewsTv = view.findViewById(R.id.num_reviews_tv);
+        numPagesTv = view.findViewById(R.id.num_pages_tv);
+        reviewsLabelTv = view.findViewById(R.id.reviews_label_tv);
+    }
 }
