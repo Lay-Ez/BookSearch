33b7edac7e2ea3e4335defa9cbd30120b5cb2ae1
Roman <deep.depp@yandex.ru>
2020-03-17 21:13:08 +0300

    add back button to the toolbar when viewing book

--- app/src/main/java/com/romanoindustries/booksearch/BookViewActivity.java
+++ app/src/main/java/com/romanoindustries/booksearch/BookViewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.romanoindustries.booksearch.viewmodels.BookViewActivityViewModel;
@@ -19,5 +20,16 @@ public class BookViewActivity extends AppCompatActivity {
         viewBookViewModel = new ViewModelProvider(this).get(BookViewActivityViewModel.class);
         String bookUrl = getIntent().getStringExtra(Intent.EXTRA_CONTENT_QUERY);
         viewBookViewModel.loadBook(bookUrl);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
